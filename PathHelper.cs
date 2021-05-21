c81740fe6f0d281a164cd8235b25a84ceafe51bb
SijyKijy <SijyKijy@users.noreply.github.com>
2021-05-21 09:59:44 +0300

    Add logic

---
+++ PathHelper.cs
@@ -0,0 +1,59 @@
+﻿using System.Diagnostics;
+
+namespace ByondPather
+{
+    public static class PathHelper
+    {
+        public static void PatchPairBytes(byte[] b, byte A0, byte A1, byte B0, byte B1, ref int i)
+        {
+            while (i < b.Length)
+            {
+                if (b[i] == A0 && b[i + 1] == A1)
+                {
+                    Debug.WriteLine($"[INFO] Pattern {A0}{A1} Found at {i}, patching with {B0}{B1}");
+
+                    b[i] = B0;
+                    b[i + 1] = B1;
+                    break;
+                }
+                i++;
+            }
+        }
+
+        public static void PatchTripletBytes(byte[] b, byte A0, byte A1, byte A2, byte B0, byte B1, byte B2, ref int i)
+        {
+            while (i < b.Length)
+            {
+                if (b[i] == A0 && b[i + 1] == A1 && b[i + 2] == A2)
+                {
+                    Debug.WriteLine($"[INFO] Pattern {A0}{A1}{A2} Found at {i}, patching with {B0}{B1}{B2}");
+
+                    b[i] = B0;
+                    b[i + 1] = B1;
+                    b[i + 2] = B2;
+                    break;
+                }
+                i++;
+            }
+        }
+
+        public static void PatchFiveBytes(byte[] b, byte A0, byte A1, byte A2, byte A3, byte A4, byte B0, byte B1, byte B2, byte B3, byte B4, ref int i)
+        {
+            while (i < b.Length)
+            {
+                if (b[i] == A0 && b[i + 1] == A1 && b[i + 2] == A2 && b[i + 3] == A3 && b[i + 4] == A4)
+                {
+                    Debug.WriteLine($"[INFO] Pattern {A0}{A1}{A2}{A4} Found at {i}, patching with {B0}{B1}{B2}{B4}");
+
+                    b[i] = B0;
+                    b[i + 1] = B1;
+                    b[i + 2] = B2;
+                    b[i + 3] = B3;
+                    b[i + 4] = B4;
+                    break;
+                }
+                i++;
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,12 +1,111 @@
 ﻿using System;
+using System.Diagnostics;
+using System.IO;
 
 namespace ByondPather
 {
-    class Program
+    public static class Program
     {
-        static void Main(string[] args)
+        private const string _fileName = "dreamseeker.exe";
+
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string path;
+
+            if (args.Length == 1)
+            {
+                path = args[0];
+            }
+            else
+            {
+                Console.WriteLine($"Enter the path file {_fileName} or drag the file and drop them in the console window:");
+                path = Console.ReadLine();
+            }
+
+            FileVersionInfo fileInfo;
+            while (!IsValidFile(path, out fileInfo))
+            {
+                Console.Write($"[Error] File {_fileName} not found.\nEnter the path again: ");
+                path = Console.ReadLine();
+            }
+
+            if (fileInfo.ProductBuildPart <= 512)
+                PathLE512(path);
+            else
+                PathGT512(path);
+        }
+
+        private static bool IsValidFile(string path, out FileVersionInfo fileInfo)
+        {
+            fileInfo = null;
+            if (!File.Exists(path))
+                return false;
+
+            fileInfo = FileVersionInfo.GetVersionInfo(path);
+            return fileInfo.OriginalFilename is _fileName;
+        }
+
+        /// <summary>
+        ///     Patch a version less than or equal to 512
+        /// </summary>
+        /// <param name="path">File path</param>
+        private static void PathLE512(string path)
+        {
+            byte[] incArray = File.ReadAllBytes(path);
+            byte[] resArray = new byte[incArray.Length];
+            
+            incArray.CopyTo(resArray, 0);
+
+            int i = 0;
+            PathHelper.PatchTripletBytes(resArray, 15, 69, 249, 137, 207, 144, ref i);
+            PathHelper.PatchPairBytes(resArray, 116, 72, 144, 144, ref i);
+            PathHelper.PatchFiveBytes(resArray, 15, 132, 68, 2, 0, 144, 144, 144, 144, 144, ref i);
+            PathHelper.PatchPairBytes(resArray, 116, 74, 144, 144, ref i);
+            PathHelper.PatchPairBytes(resArray, 116, 63, 144, 144, ref i);
+            PathHelper.PatchPairBytes(resArray, 116, 14, 144, 144, ref i);
+            PathHelper.PatchPairBytes(resArray, 116, 14, 144, 144, ref i);
+            PathHelper.PatchPairBytes(resArray, 116, 79, 144, 144, ref i);
+
+            int num = 0;
+            for (int j = 0; j < incArray.Length; j++)
+            {
+                if (incArray[j] != resArray[j])
+                    num++;
+            }
+
+            Console.WriteLine("Bytes changed = " + num);
+            File.WriteAllBytes(path, resArray);
+
+            Console.WriteLine("Pathed. Press any key to close this window...");
+            Console.ReadLine();
+        }
+
+        /// <summary>
+        ///     Patch a version greater than 512
+        /// </summary>
+        /// <param name="path">File path</param>
+        private static void PathGT512(string path)
+        {
+            byte[] incArray = File.ReadAllBytes(path);
+            byte[] resArray = new byte[incArray.Length];
+
+            incArray.CopyTo(resArray, 0);
+
+            int i = 0;
+            PathHelper.PatchFiveBytes(resArray, 191, 30, 0, 0, 0, 191, 0, 0, 0, 0, ref i);
+
+            int num = 0;
+            for (int j = 0; j < incArray.Length; j++)
+            {
+                if (incArray[j] != resArray[j])
+                    num++;
+            }
+
+            Console.WriteLine("Bytes changed = " + num);
+            File.WriteAllBytes(path, resArray);
+
+            Console.WriteLine("Pathed. Press any key to close this window...");
+            Console.ReadLine();
         }
     }
 }
