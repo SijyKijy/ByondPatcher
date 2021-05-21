23adce19f318ca8883c4594e306e5c9044ee4dc6
SijyKijy <SijyKijy@users.noreply.github.com>
2021-05-21 08:45:27 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ByondPather
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
